9811b1851f15fcc79260ddeb2b41b200ac5f1e6b
judgegc <judgegc@gmail.com>
2018-02-14 10:23:52 +0200

    Add thread count to mem cmd

--- src/commands/memory-usage-command.js
+++ src/commands/memory-usage-command.js
@@ -1,4 +1,8 @@
+const childProcess = require('child_process');
+const os = require('os');
 const bytes = require('bytes');
+
+const Util = require('./../util');
 const settings = require('./../../settings.json');
 
 class MemoryUsageCommand {
@@ -6,12 +10,30 @@ class MemoryUsageCommand {
         this.args = args;
     }
 
-    execute(client, msg) {
+    async execute(client, msg) {
         const memUsage = process.memoryUsage();
+        const totalThreads = await (async () => {
+            return new Promise((resolve, reject) => {
+                switch (os.platform()) {
+                    case 'linux':
+                        childProcess.exec('ps -eo nlwp | tail -n +2 | awk \'{ num_threads += $1 } END { print num_threads }\'', (error, stdout, stderr) => {
+                            if (error) {
+                                resolve('unknown');
+                            }
+                            resolve(stdout);
+                        });
+                        break;
+                    default:
+                        resolve('unknown');
+                }
+            });
+        })();
+
         return new Promise((resolve, reject) => {
             resolve(Object.keys(memUsage).map(p => `${p}: ${bytes(memUsage[p])}`)
                 .concat(`sandboxLimit: ${bytes(settings['js-sandbox']['memory-limit'] * 1024 * 1024)}`)
-                .concat(`pool size: ${settings['js-sandbox']['instances']}`)
+                .concat(`totalThreads: ${totalThreads}`)
+                .concat(`pool size: ${settings['js-sandbox']['instances']}`)              
                 .join('\n'));
         });
     }
