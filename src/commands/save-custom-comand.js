a0595575634e7c473620ff9814fd584c6b9a5d62
judgegc <judgegc@gmail.com>
2018-05-04 00:00:19 +0300

    Fix possibility to override someone else's custom command

--- src/commands/save-custom-comand.js
+++ src/commands/save-custom-comand.js
@@ -11,10 +11,22 @@ class SaveCustomCommand {
             return `Error. Custom command max length is: ${COMMAND_MAX_LENGTH}`;
         }
 
+        const cmdProc = Services.resolve('commandprocessor');
+
         try {
-            await Services
-                .resolve('commandprocessor')
-                .createCommand(msg.guild.id, this.args[0], this.args[1], msg.author.id, this.args[2]);
+            const commands = cmdProc.getCustomCommands(msg.guild.id);
+
+            if (commands !== undefined && commands.size > 0) {
+                const name = this.args[0];
+                const foundCmd = commands.get(name);
+
+                if (foundCmd && msg.author.id !== foundCmd.owner) {
+                    const ownerObj = client.users.get(foundCmd.owner);
+                    throw new Error(`Command '${name}' already owned by ${ownerObj ? ownerObj.username : foundCmd.owner}`);
+                }
+            }
+
+            await cmdProc.createCommand(msg.guild.id, this.args[0], this.args[1], msg.author.id, this.args[2]);
         } catch (e) {
             return e.message;
         }
