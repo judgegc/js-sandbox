17fb5ef240c0fb038692f37c79864c200f08adb5
judgegc <judgegc@gmail.com>
2018-07-19 23:00:08 +0300

    Fix command detection regex

--- src/message-parser.js
+++ src/message-parser.js
@@ -5,14 +5,14 @@ const Util = require('./util');
 class MessageParser {
     constructor() {
         const prefix = Util.escapeRegExp(settings['commands-prefix']);
-        this.cmdPattern = new RegExp(`^${prefix}(\\w+)(?: ([\\w\\W]+))?`);
+        this._cmdPattern = new RegExp(`^${prefix}(\\w+)(?: ([\\w\\W]+))?$`);
         this._argsParser = new ArgsParser();
     }
     parse(msg) {
         let found = null;
         if (found = msg.match(/```([a-zA-Z]*)\n([\s\S]*?)\n```/)) {
             return { type: 'source_code', language: found[1], content: found[2] };
-        } else if (found = msg.match(this.cmdPattern)) {
+        } else if (found = msg.match(this._cmdPattern)) {
             return { type: 'command', name: found[1], args: this._argsParser.parse(found[2]) };
         }
         return { type: 'unknown', content: msg };
